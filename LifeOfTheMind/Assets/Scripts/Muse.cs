7d034a48016c90b1d88b931493d8ee417dc62bc3
eurbs <emileeurbanek@gmail.com>
2016-04-17 03:15:11 -0500

    after a long and tiring journey through computers with busted bluetooth cards to weird casting issues in C#, acceleration now works when spinning the camera with your head

--- LifeOfTheMind/Assets/Scripts/Muse.cs
+++ LifeOfTheMind/Assets/Scripts/Muse.cs
@@ -3,35 +3,38 @@ using System.Collections;
 using System;
 using SharpOSC;
 
+/* This class is never instantiated, so everything is a static function */
 public class Muse : MonoBehaviour {
 
 	public UDPListener listener;
 
 	/* muse information available to other scripts */
 
-	/* acc(0) == up/down
-	 * y+ == down
-	 * y- == up 
-	*/
-	/* acc(2) = left/right
-	 * x+ == right
-	 * x- == left
-	 */
-	public static float acc_x = 0;
-	public static float acc_y = 0;
-	//TODO(emmi): consider adding acc_z (acc(1)), unlikely that we'll use
 	public static int blinks = 0;
 	public static int clenches = 0;	// Jaw clench count
-	public static string mood = "content";
 
-	/* headset data */
-	public static int battery = 0;
+
+	/* Constants for use with accelerometer data. */
+	private const int UD = 0;		// Array access for up/down
+	private const int FB = 1;		// Array access for front/back
+	private const int LR = 2;		// Array access for left/right
+
+	/* Accelerometer data. Listed in order of { UD, FB, LR }
+	*  	UD: head up/down
+	* 		FB: head front/back
+	* 		LR: head left/right
+	*/
+	private static float[] acc_dt = { 0f, 0f, 0f };				// Delta time if min is below threshold and max is above.
+	private static float[] acc_threshold = { 300f, 0f, 200f };	// Start detecting acceleration of head
+	private static float[] acc_recent = { 0f, 0f, 0f };			// Most recently recorded position data
 
 	void Start() 
 	{
-		// Callback function for received OSC messages. 
+		// Initialize variables
+
 		print("starting muse serv");
 
+		// Callback function for received OSC messages.
 		HandleOscPacket callback = delegate(OscPacket packet)
 		{
 			var messageReceived = (OscMessage)packet;
@@ -39,25 +42,15 @@ public class Muse : MonoBehaviour {
 
 			if(addr == "/muse/acc") {
 				//head position and accleration
-				float temp_acc_y = (float)messageReceived.Arguments[0];
-				if (temp_acc_y < -300.0) {
-					//head up
-					acc_y = temp_acc_y;
-				} else if (temp_acc_y > 300.0) {
-					//head down
-					acc_y = temp_acc_y;
-				}
-				float temp_acc_x = (float)messageReceived.Arguments[2];
-				if (temp_acc_x < -200.0) {
-					//head left
-					acc_x = temp_acc_x;
-				} else if (temp_acc_x > 200.0) {
-					//head right
-					acc_x = temp_acc_x;
-				}
+				float[] acc_data = { 
+					(float)messageReceived.Arguments[0],
+					(float)messageReceived.Arguments[1],
+					(float)messageReceived.Arguments[2]
+				};
+				CallbackAcc(acc_data);
 			}
 
-			if(addr == "/muse/elements/blink") {
+			else if(addr == "/muse/elements/blink") {
 				int blink = (int)messageReceived.Arguments[0];
 				if (blink == 1) {
 					blinks++;
@@ -65,8 +58,8 @@ public class Muse : MonoBehaviour {
 			}
 				
 			else if (addr == "/muse/elements/jaw_clench") {
-				bool clench = (bool) messageReceived.Arguments[0];
-				if (clench) {
+				int clench = (int) messageReceived.Arguments[0];
+				if (clench == 1) {
 					clenches++;
 				}
 			}
@@ -100,6 +93,7 @@ public class Muse : MonoBehaviour {
 					battery = (int)messageReceived.Arguments[0];
 				}
 			}
+		};
 
 		// Create an OSC server.
 		listener = new UDPListener(5000, callback);
@@ -108,16 +102,81 @@ public class Muse : MonoBehaviour {
 
 	void Update() 
 	{
-		clenches = 0;
 		//reset public muse data
 		blinks = 0;
-		acc_y = 0;
-		acc_x = 0;
+		clenches = 0;
 	}
 
 	void OnApplicationQuit() 
 	{
 		listener.Close();
 	}
-}	
 
+	/*************************/
+	/* PRIVATE CLASS METHODS */
+	/*************************/
+
+	/* Called when accelerometer data comes in from the accelerometer on the Muse.
+	 * This updates variables accordingly.
+	 *
+	 * I intend to describe this further, but it's a hackathon project so...
+	 * let's be real, I probably won't.
+	 */
+	private static void CallbackAcc(float[] acc_data)
+	{
+		for (int i = 0; i < 3; i++) {
+			acc_recent[i] = acc_data[i];
+			if (Math.Abs(acc_data[i]) > acc_threshold[i]) {
+				acc_dt[i] += 1.0f;
+			} else {
+				acc_dt[i] = 0f;
+			}
+
+			if (i == LR) {
+				print ("data");
+				print (acc_data [i]);
+				print (acc_dt [i]);
+				print ("recent");
+				print (acc_recent [i]);
+			}
+		}
+
+	}
+
+	// note: now I know why Input.GetAxis() uses string args.
+	public static float GetVelocityUpDown()
+	{
+		// up is negative and down is positive which is counterintuitive,
+		// so here we switch the sign
+		float velocity = 0f;
+		if (acc_dt[UD] > 0.0) {
+			// Calculate slope
+			velocity = -1.0f * Math.Abs((acc_recent[UD] - acc_threshold[UD]) / acc_dt[UD]);
+		}
+		return velocity;
+	}
+
+	public static float GetVelocityFrontBack()
+	{
+		// Front is negative and back is positive which is counterintuitive,
+		// so here we switch the sign
+		float velocity = 0f;
+		if (acc_dt[FB] > 0.0) {
+			// Calculate slope
+			velocity = -1.0f * Math.Abs((acc_recent[FB] - acc_threshold[FB]) / acc_dt[FB]);
+		}
+		return velocity;
+	}
+
+	public static float GetVelocityLeftRight()
+	{
+		// Left is negative and right is positive which is intuitive,
+		// so here we leave the sign as is.
+		float velocity = 0f;
+		if (acc_dt[LR] > 0.0) {
+			// Calculate slope
+			velocity = (acc_recent[LR] - acc_threshold[LR]) / acc_dt[LR];
+		}
+		return velocity;
+	}
+}	
