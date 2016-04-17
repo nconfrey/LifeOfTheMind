1921474165710323af5ec7dbed95c268313804ed
Sydney Ko <kosydney@uchicago.edu>
2016-04-17 11:39:04 -0500

    real things

---
+++ LifeOfTheMind/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+/*
+ * Simple Timer Code for Load Screen
+ */
+
+public class LoadLevel : MonoBehaviour {
+
+	public float delayTime = 5; 
+
+	IEnumerator Start (){
+	
+		yield return new WaitForSeconds (delayTime);
+
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
